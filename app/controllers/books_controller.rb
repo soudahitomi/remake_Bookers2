5a0a4c084018ec658dee0bd05f61b6621aa3535c
HitomiSouda <154737068+soudahitomi@users.noreply.github.com>
2024-09-14 09:12:02 +0000

    books,user controller作成

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,19 @@
+class BooksController < ApplicationController
+  def index
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def show
+  end
+
+  def destroy
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,17 @@
 Rails.application.routes.draw do
+  get 'users/index'
+  get 'users/edit'
+  get 'users/update'
+  get 'users/show'
+  get 'books/index'
+  get 'books/create'
+  get 'books/edit'
+  get 'books/update'
+  get 'books/show'
+  get 'books/destroy'
   devise_for :users
   root to: 'homes#top'
+  # resources :books [:index, :create, :edit, :update, :show, :destroy]
+  # resources :users [:show, :index, :update, :edit]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
