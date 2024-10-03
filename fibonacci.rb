b38c70b5e5d8969566f4a5da1a56e6d2aa41b7b4
HitomiSouda <154737068+soudahitomi@users.noreply.github.com>
2024-10-03 12:07:35 +0000

    フィボナッチ数列完成

--- fibonacci.rb
+++ fibonacci.rb
@@ -3,16 +3,8 @@ def fibonacci(num)
   @memo[num] ||= fibonacci(num - 1) + fibonacci(num - 2)
 end
 
-# def fibonacci(num)再帰的処理に時間かかる
-#   if num == 0
-#     return 0
-#   elsif num == 1
-#     return 1
-#   else
-#     fibonacci(num - 1) + fibonacci(num - 2)
-#   end
-# end
-
   (0..20).each do |i|
-    puts fibonacci(i)
+    result = fibonacci(i)
+    break if result > 10000
+    puts result
   end
