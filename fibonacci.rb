67e501a43f853cc1219dec38c4e9567f5f95e05e
HitomiSouda <154737068+soudahitomi@users.noreply.github.com>
2024-10-06 10:23:48 +0000

    フィボナッチ短くした

--- fibonacci.rb
+++ fibonacci.rb
@@ -1,10 +1,17 @@
-@memo = [ 0, 1 ]
-def fibonacci(num)
-  @memo[num] ||= fibonacci(num - 1) + fibonacci(num - 2)
-end
+# @memo = [ 0, 1 ]
+# def fibonacci(num)
+#   @memo[num] ||= fibonacci(num - 1) + fibonacci(num - 2)
+# end
 
-  (0..20).each do |i|
-    result = fibonacci(i)
-    break if result > 10000
-    puts result
-  end
+#   (0..20).each do |i|
+#     result = fibonacci(i)
+#     break if result > 10000
+#     puts result
+#   end
+a, b, c = 0, 0, 1
+
+while a <= 10000
+  puts a
+
+  a, b, c = b, c, a + b + c
+end
