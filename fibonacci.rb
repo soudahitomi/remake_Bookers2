f5bd5746ccc8b43b7eae61709c45472ad9f2e6d2
HitomiSouda <154737068+soudahitomi@users.noreply.github.com>
2024-10-08 04:49:49 +0000

    FizzBuzzプログラムを修正

--- fibonacci.rb
+++ fibonacci.rb
@@ -8,10 +8,35 @@
 #     break if result > 10000
 #     puts result
 #   end
-a, b, c = 0, 0, 1
+# a, b, c = 0, 0, 1
 
-while a <= 10000
-  puts a
+# while a <= 10000
+#   puts a
 
-  a, b, c = b, c, a + b + c
+#   a, b, c = b, c, a + b + c
+# end
+
+
+def fizzbuzz(number)
+  if number % 15 == 0
+    "FizzBuzz"
+  elsif number % 3 == 0
+    "Fizz"
+  elsif number % 5 == 0
+    "Buzz"
+  else
+    number.to_s
+  end
+end
+
+puts "数字を入力してください"
+
+input = gets.chomp
+
+if input.match?(/\A-?\d+\z/)
+  number = input.to_i
+  puts "結果は…"
+  puts fizzbuzz(number)
+else
+  puts "エラーです。数字を入力してください"
 end
